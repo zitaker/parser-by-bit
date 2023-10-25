1fe66f0a080d7b3723d8df0640097d9ba82693d6
georg <georbearwolf.com>
2023-10-25 16:19:56 +0300

    output the contents of the href

--- parse/main.py
+++ parse/main.py
@@ -21,26 +21,72 @@
 
 # ___________________________________________________
 # берет адрес ссылки, но не обходит защиту ссылок
-from selenium import webdriver
-from bs4 import BeautifulSoup
-
-
-driver = webdriver.Chrome()
-driver.get("https://announcements.bybit.com/en-US/article/special-offer-looking-for-a-new-crypto-card--bltaa691019e4782034/")
-
-html = driver.page_source
-soup = BeautifulSoup(html, "html.parser")
-
-for tag in soup.find_all('div', class_='article-list'):
-    print(tag)
-    # href = tag['href']
-    # if not href:
-    #     print('нет тегов')
-    # print(f"https://announcements.bybit.com{href}")
-
-driver.quit()
+# from selenium import webdriver
+# from bs4 import BeautifulSoup
+#
+#
+# driver = webdriver.Chrome()
+# driver.get("https://announcements.bybit.com/en-US/article/special-offer-looking-for-a-new-crypto-card--bltaa691019e4782034/")
+#
+# html = driver.page_source
+# soup = BeautifulSoup(html, "html.parser")
+#
+# for tag in soup.find_all('div', class_='article-list'):
+#     print(tag)
+#     # href = tag['href']
+#     # if not href:
+#     #     print('нет тегов')
+#     # print(f"https://announcements.bybit.com{href}")
+#
+# driver.quit()
 # берет адрес ссылки, но не обходит защиту ссылок
 # ___________________________________________________
 
+# _______________________________________________
+# модель как должно выгдлядить
+
+# from selenium import webdriver
+# import time
+#
+#
+# url = "https://python-scripts.com/"
+# driver = webdriver.Chrome()
+#
+# try:
+#     driver.get(url=url)
+#     time.sleep(3)
+# except Exception as ex:
+#     print(ex)
+# finally:
+#     driver.close()
+#     driver.quit()
+
+# модель как должно выгдлядить
+# ________________________________________
 
 
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+url = "https://python-scripts.com/"
+# url = 'https://announcements.bybit.com/en-US/?category=&page=1'
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+
+try:
+    driver.get(url=url)
+    search_field = driver.find_element(By.CLASS_NAME, 'site-title')
+    # search_field = driver.find_element(By.CLASS_NAME, 'no-style')
+    print(search_field.text)
+
+    # вывести содержимое href
+    # element = driver.find_element(By.LINK_TEXT, 'a')
+    # href = element.get_property('href')
+    # print(href)
+except Exception as ex:
+    print(ex)
+finally:
+    driver.close()
+    driver.quit()
