b66217e1056f54e52645b5d1d1bf03f655614a2a
georg <georbearwolf.com>
2023-10-27 17:07:29 +0300

    get all the necessary elements from the entire page

--- parse/main.py
+++ parse/main.py
@@ -3,20 +3,29 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
-
+from bs4 import BeautifulSoup
+import requests
 def parser(urls):
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
     try:
         driver.get(url=urls)
-        tag_a = driver.find_element(By.CLASS_NAME, 'no-style')
+        tag_a = driver.find_elements(By.CLASS_NAME, 'no-style')
 
-        tag_div = tag_a.find_element(By.CLASS_NAME, 'article-item-date')
-        tag_span = tag_a.find_element(By.TAG_NAME, 'span')
-        tag_a_href = tag_a.find_element(
-            By.XPATH, "//a[@class='no-style']").get_attribute('href')
+        # tag_div = tag_a.find_element(By.CLASS_NAME, 'article-item-date')
+        # tag_span = tag_a.find_element(By.TAG_NAME, 'span')
+        # tag_a_href = tag_a.find_element(
+        #     By.XPATH, "//a[@class='no-style']").get_attribute('href')
+        #
+        # return tag_div.text, tag_span.text, tag_a_href
 
-        return tag_div.text, tag_span.text, tag_a_href
+        all_tags = []
+        for tag in tag_a:
+            all_tags.append(tag.text)
+        return all_tags
+
+        # with open(file_path, "w") as file:
+        #     file.write(driver.page_source)
     except Exception as ex:
         return ex
     finally:
@@ -24,8 +33,26 @@ def parser(urls):
         driver.quit()
 
 
+def get_items_urls(file_path):
+    with open(file_path) as file:
+        src = file.read()
+
+    soup = BeautifulSoup(src, "lxml")
+    items_divs = soup.find_all("div", class_="article-item-date")
+
+    result = []
+    for elem in items_divs:
+        result.append(elem.text)
+    return result
+
 url = 'https://announcements.bybit.com/en-US/?category=&page=1'
+file_path = '../tests/fixtures/source-page.html'
 
-print(parser(url))
+def main():
+    result = parser(url)
+    print(result)
+    # print(get_items_urls(file_path))
 
 
+if __name__ == "__main__":
+    main()
