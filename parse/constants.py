5a4b4280b8b86e50c854c54d3bb36ee750093392
georg <georbearwolf.com>
2023-10-28 18:02:27 +0300

    implement checking for existing data and adding only new data

--- parse/constants.py
+++ parse/constants.py
@@ -1,1 +1,2 @@
 URLS = 'https://announcements.bybit.com/en-US/?category=&page=1'
+DIRECTORY_PATH = '../tests/fixtures/data.csv'
--- parse/main.py
+++ parse/main.py
@@ -1,8 +1,10 @@
+import csv
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-from constants import URLS
+from constants import URLS, DIRECTORY_PATH
 
 
 def parser(urls):
@@ -12,13 +14,14 @@ def parser(urls):
         driver.get(url=urls)
 
         tag_a = driver.find_elements(By.XPATH, "//a[@class='no-style']")
-        result = []
+        data = []
         for tag in tag_a:
             tag_div = tag.find_element(By.CLASS_NAME, 'article-item-date')
             tag_span = tag.find_element(By.TAG_NAME, 'span')
-            result.append(
-                f"{tag_div.text} {tag_span.text} {tag.get_attribute('href')}")
-        return result
+            data.append(
+                [tag_div.text, tag_span.text, tag.get_attribute('href')])
+
+        return data
     except Exception as ex:
         return ex
     finally:
@@ -26,9 +29,16 @@ def parser(urls):
         driver.quit()
 
 
+def saving_data(data):
+    with open(DIRECTORY_PATH, 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerows(data)
+
+
 def main():
-    result = parser(URLS)
-    print(result)
+    data = parser(URLS)
+    saving_data(data)
+    print(data)
 
 
 if __name__ == "__main__":
