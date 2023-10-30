03e66561b5fbbb9b3e322b4353a5dd37d0669dd6
georg <georbearwolf.com>
2023-10-30 10:50:26 +0300

    output the answer to the search in lists

--- parse/main.py
+++ parse/main.py
@@ -37,35 +37,48 @@ import time
         #         writer.writerows(data)
 
 
-def test():
-    output_format = "%Y-%m-%d %H:%M:%S"
+def parser(urls):
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+
+    try:
+        driver.get(url=urls)
+        current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+        tag_a = driver.find_elements(By.XPATH, "//a[@class='no-style']")
+        data = []
+        for tag in tag_a:
+            tag_span = tag.find_element(By.TAG_NAME, 'span')
+            data.append(
+                [current_time, tag_span.text, tag.get_attribute('href')])
+
+        return data
+    except Exception as ex:
+        return ex
+    finally:
+        driver.close()
+        driver.quit()
+
+
+def main():
     while True:
         # получил данные и добавил текущее время
-        # current_time = datetime.now().strftime(output_format)
-        current_time = '1'
+        # data = parser(URLS)
+        data = '1'
 
         # открыл данные для сравнения
         with open(TEMPORARY_DATA, 'r') as file:
             content = file.read()
             file.close()
 
-
             # сравниваю данные по ссылке или заголовку
         # for elem in content:
-        if current_time not in content:
-            print('сохранить данные в temporary_data')
-            # СОХРАНИТЬ ВРЕМЕННЫЕ ФАЙЛЫ в temporary_data
-            with open(TEMPORARY_DATA, 'w', newline='') as file:
-                writer = csv.writer(file)
-                writer.writerows('3')   # новые данные
-                file.close()
-                time.sleep(1)
-        else:
+        # for title in data:
+        if data not in content:
             # если данные отличаются сохранить данные в new_data и temporary_data
             print('данных нет в файле - сохранить данные в temporary_data и new_data')
             with open(TEMPORARY_DATA, 'w', newline='') as file:
                 writer = csv.writer(file)
-                writer.writerows('2')   # новые данные
+                writer.writerows('1')   # новые данные
                 file.close()
 
             # сохранить полученные данные в new_data
@@ -74,6 +87,16 @@ def test():
                 writer.writerows('3')  # новые данные
                 file.close()
                 time.sleep(1)
+        else:
+            print('сохранить данные в temporary_data')
+            # СОХРАНИТЬ ВРЕМЕННЫЕ ФАЙЛЫ в temporary_data
+            with open(TEMPORARY_DATA, 'w', newline='') as file:
+                writer = csv.writer(file)
+                writer.writerows('1')  # новые данные
+                file.close()
+                time.sleep(1)
+
+        print(main())
 
 
 
@@ -81,10 +104,10 @@ def test():
 
 
 
-def main():
-    # data = parser(URLS)
-    # saving_data(data)
-    print(test())
+# def main(urls):
+#     # data = parser(URLS)
+#     # saving_data(data)
+#     print(test(urls))
 
 
 if __name__ == "__main__":
