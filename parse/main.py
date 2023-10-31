9da3ab6e34aec549dbb899df0fa19532ec62e51e
georg <georbearwolf.com>
2023-10-31 14:21:47 +0300

    remove repetitions

--- parse/main.py
+++ parse/main.py
@@ -1,45 +1,23 @@
 import csv
+import time
 
+from datetime import datetime
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from constants import URLS, NEW_DATA, TEMPORARY_DATA
 
-from datetime import datetime
-import time
-
-
-        # def parser(urls):
-        #     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-        #
-        #     try:
-        #         driver.get(url=urls)
-        #
-        #         tag_a = driver.find_elements(By.XPATH, "//a[@class='no-style']")
-        #         data = []
-        #         for tag in tag_a:
-        #             # tag_div = tag.find_element(By.CLASS_NAME, 'article-item-date')
-        #             tag_span = tag.find_element(By.TAG_NAME, 'span')
-        #             # data.append([tag_div.text, tag_span.text, tag.get_attribute('href')])
-        #             data.append([tag_span.text, tag.get_attribute('href')])
-        #
-        #         return data
-        #     except Exception as ex:
-        #         return ex
-        #     finally:
-        #         driver.close()
-        #         driver.quit()
-        #
-        #
-        # def saving_data(data):
-        #     with open(TEMPORARY_DATA, 'w', newline='') as file:
-        #         writer = csv.writer(file)
-        #         writer.writerows(data)
-
 
 def parser(urls):
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    options = webdriver.ChromeOptions()
+    options.add_argument(
+        "user-agent=Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:84.0) "
+        "Gecko/20100101 Firefox/84.0")
+    options.add_argument("--headless")
+
+    driver = webdriver.Chrome(
+        options=options, service=Service(ChromeDriverManager().install()))
 
     try:
         driver.get(url=urls)
